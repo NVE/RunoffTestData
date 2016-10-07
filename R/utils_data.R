e302a6d5502c4177f74438c0428bd49e9663ea2f
jmgnve <jmg@nve.no>
2016-10-07 14:56:52 +0200

    Added function for reading Jhydro data

--- R/utils_data.R
+++ R/utils_data.R
@@ -31,6 +31,41 @@ load_runoff_obs <- function(path_obs = "Period_Valid") {
 }
 
 
+# Read simulated discharge from Jhydro models
+
+load_jhydro_res <- function(path_model) {
+  
+  files_data <- list.files(path_model)
+  
+  files_data <- sort(files_data)
+  
+  list_data <- vector("list", length(files_data))
+  
+  for (ifile in 1:length(files_data)) {
+    
+    tmp <- read.table(paste(path_model, "/", files_data[ifile], sep = ""), header = TRUE)
+    
+    list_data[[ifile]]$q_sim <- tmp$q_sim
+    
+  }
+  
+  q_mat <- sapply(list_data, function(tmp) tmp$q_sim)
+  
+  q_sim <- data.frame(as.Date(tmp$date), q_mat)
+  
+  station_names <- gsub("_station.txt", "", files_data)
+  
+  station_names <- gsub("_", ".", station_names)
+  
+  colnames(q_sim) <- c("Time", station_names)
+  
+  return(q_sim)
+  
+}
+
+
+
+
 
 
 

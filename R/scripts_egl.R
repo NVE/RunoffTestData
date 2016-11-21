d5d3ec5de870df9812df2411fb6aa73ba9381f25
jmgnve <jmg@nve.no>
2016-11-21 12:19:07 +0100

    Adapted elins script

--- R/scripts_egl.R
+++ R/scripts_egl.R
@@ -1,112 +1,80 @@
 
-# Run evaluation for hbv results
+##############################################################################################
 
-run_eval_hbv <- function(path_testdata = "C:/Users/jmg/Dropbox/Work/Rcode/RunoffTestData") {
-  
-  # Set working directory to correct folder
-  
-  setwd(path_testdata)
-  
-  # Libraries
-  
-  library(tcltk)
-  
-  # Source files
-  
-  source("R/run_evaluation.R")
-  source("R/utils_eval.R")
-  source("R/utils_data.R")
-  
-  # Path to model results
-  
-  path_model <- tk_choose.dir(default = "C:/Users/jmg/Dropbox/Work/VannData/")
-  
-  # Describe model
-  
-  model_name <- readline("Name of the model? ")
-  model_version <- readline("Version of the model? ")
-  model_desc <- readline("Additional description? ")
-  model_input <- readline("Input data? ")
-  model_res <- readline("Path to model results? ")
-  
-  ### Run analysis for calibration period ###
-  
-  period <- "calib"
-  
-  # Path to simulation results
-  
-  path_obs <- paste("24h/qobs_", period, sep = "")
-  
-  path_sim <- paste(path_model, "/", period, "_txt", sep = "")
-  
-  # Load observed runoff
-  
-  q_obs <- load_runoff_obs(path_obs)
-  
-  # Load simulated runoff
-  
-  q_sim <- load_hbv_res(path_sim)
-  
-  ikeep <- which(colnames(q_sim) %in% colnames(q_obs))
-  
-  q_sim <- q_sim[, ikeep]
-  
-  # Run analysis
-  
-  run_evaluation(q_obs, q_sim, model_name, model_version, model_desc, model_input, model_res, period)
-  
-  
-  ### Run analysis for validation period ###
-  
-  period <- "valid"
-  
-  # Path to simulation results
-  
-  path_obs <- paste("24h/qobs_", period, sep = "")
-  
-  path_sim <- paste(path_model, "/", period, "_txt", sep = "")
-  
-  # Load observed runoff
-  
-  q_obs <- load_runoff_obs(path_obs)
-  
-  # Load simulated runoff
-  
-  q_sim <- load_hbv_res(path_sim)
-  
-  ikeep <- which(colnames(q_sim) %in% colnames(q_obs))
-  
-  q_sim <- q_sim[, ikeep]
-  
-  # Run analysis
-  
-  run_evaluation(q_obs, q_sim, model_name, model_version, model_desc, model_input, model_res, period)
-  
-}
+# Clear workspace
+
+rm(list=ls())
+
+# Source required functions
+
+source("R/run_evaluation.R")
+source("R/utils_eval.R")
+source("R/utils_data.R")
+
+# Add information about your model
+
+model_name <- "HBV"
+model_version <- "operational"
+model_desc <- "The is a toy example"
+model_input <- "SeNorge"
+model_res <- "Local folder on my computer"
+
+# Path to observation data and simulation results
+
+path_sim <- "//hdata/fou/jmg/hbv/"
 
 
+##############################################################################################
+
+# Run analysis for calibration period
+
+period <- "calib"
+
+path_obs <- "24h/qobs_calib"
+
+# Load observed runoff
+
+q_obs <- load_runoff_obs(path_obs)
+
+# Load simulated runoff (only keep those who match observations)
+
+path_tmp <- file.path(path_sim, paste(period, "_txt", sep = ""), "mmday")
+
+q_sim <- load_hbv_res(path_tmp)
+
+ikeep <- which(colnames(q_sim) %in% colnames(q_obs))
+
+q_sim <- q_sim[, ikeep]
+
+# Run analysis
+
+run_evaluation(q_obs, q_sim, model_name, model_version, model_desc, model_input, model_res, period)
 
 
+##############################################################################################
 
+# Run analysis for calibration period
 
+period <- "valid"
 
+path_obs <- "24h/qobs_valid"
 
+# Load observed runoff
 
+q_obs <- load_runoff_obs(path_obs)
 
+# Load simulated runoff (only keep those who match observations)
 
+path_tmp <- file.path(path_sim, paste(period, "_txt", sep = ""), "mmday")
 
+q_sim <- load_hbv_res(path_tmp)
 
+ikeep <- which(colnames(q_sim) %in% colnames(q_obs))
 
+q_sim <- q_sim[, ikeep]
 
+# Run analysis
 
-
-
-
-
-
-
-
-
-
+run_evaluation(q_obs, q_sim, model_name, model_version, model_desc, model_input, model_res, period)
 
 
